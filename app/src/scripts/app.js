dd12c269ff8260e6456d4beb62c743b5289a4462
pvds <pvdsteen@gmail.com>
2017-06-15 21:17:51 +0200

    Get and show contact

--- app/src/scripts/app.js
+++ app/src/scripts/app.js
@@ -6,47 +6,39 @@ document.addEventListener('DOMContentLoaded', () => {
     /**
      * Initialize global variables
      **/
+
     pcard.initVars();
-    console.dir(pcard);
-    getContactList();
 
     /**
      * Listen for triggers
      **/
+
     pcard.triggers.contact.edit.addEventListener('click', () => editContact(pcard.triggers.contact.select));
     pcard.triggers.contact.save.addEventListener('click', () => saveContact(pcard.triggers.contact.select));
     pcard.form.fields.note.addEventListener('keyup', () => autogrow(pcard.form.fields.note, 20));
 
     /**
-     * Initialize favorite click listener
+     * Initialize theme functions
      **/
+
+    // favorite click listener
     toggleFavorite();
 
-    /**
-     * Initialize toggle between fav and all contacts
-     **/
+    // toggle between fav and all contacts
     toggleListView();
 
-    /**
-     * Initialize toggle between contact list and contact details
-     **/
+    // toggle between contact list and contact details
     toggleView();
 
-    /**
-     * Initialize favorite contact list filter
-     **/
+    // favorite contact list filter
     filterContactList();
 
-    /**
-     * Set textarea height to content height
-     **/
+    // Set textarea height to content height
     autogrow(pcard.form.fields.note);
 });
 
 window.addEventListener('resize', () => {
-    /**
-     * Set textarea height to content height
-     **/
+    // Set textarea height to content height
     autogrow(pcard.form.fields.note);
 });
 
--- app/src/scripts/components/functions.js
+++ app/src/scripts/components/functions.js
@@ -72,41 +72,188 @@ const toggleFavorite = () => {
 
 // TODO: rename to all 'add' instances to 'create'
 const addContact = () => {
-    console.log('adding new contact!');
+    // console.log('adding new contact!');
 };
 
-const showContact = () => {
-    console.log('showing contact!');
+const showContact = (contactData, contactId) => {
+    // console.log(`showing contact ${contactId}!`);
+    // console.dir(contactData);
+
+    /**
+     * Prepare contact data
+     * */
+    const placeholder = 'placeholder';
+    const image = contactData.image !== '' ? contactData.image : 'avatar-placeholder';
+    const imageAction = contactData.image !== '' ? 'edit' : 'add';
+    // TODO: check why double newline character is not replaced by the first replace method
+    const note = contactData.note.replace('\\n', '&#xA;').replace('\\n\\n', '&#xA;&#xA;');
+
+    /**
+     * Setup markup snippet
+     * */
+    const markupDetail = `
+    <header id="contact-details-header">
+        <div class="form-control has-image">
+        <label for="contact-details-image-field">
+            <img src='dist/images/${image}.jpg' width='64' height='64' srcset='dist/images/${image}@2x.jpg 2x'/>
+            <span id='edit-image-field' class='is-hidden'>${imageAction}<br/>photo</span>
+        <?php } ?>
+    </label>
+    <input disabled hidden id="contact-details-image-field" type="file"/>
+        </div>
+        <div class="form-control has-name">
+        <input disabled id="contact-details-name-field" type="text" title="contact name" value="${contactData.name}"/>
+        </div>
+        <div class="form-control has-favorite">
+        <label for="contact-details-favorite-field">
+            <svg ${contactData.favorite ? 'hidden' : ''} class="icon-star-outline unchecked" data-name="Star outline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><title>icons</title><path d="M39.916,47.6L25.071,39.8,10.226,47.6l2.835-16.53L1.052,19.366l16.6-2.412L25.071,1.915l7.423,15.039,16.6,2.412L37.081,31.073ZM25.071,37.975l12.7,6.678L35.347,30.51,45.623,20.493l-14.2-2.064L25.071,5.56,18.719,18.429l-14.2,2.064L14.794,30.51,12.369,44.653Z" fill="#FFF"/></svg>
+            <svg ${contactData.favorite ? '' : 'hidden'} class="icon-star-filled checked" data-name="Star filled" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><title>icons</title><path d="M39.986,47.6L25.141,39.8,10.3,47.6l2.835-16.53L1.122,19.366l16.6-2.412L25.141,1.915l7.423,15.039,16.6,2.412L37.151,31.073Z" fill="#676767"/></svg>
+        </label>
+        <input disabled hidden id="contact-details-favorite-field" name="contact-details-favorite" type="checkbox" ${contactData.favorite ? 'checked' : ''}/>
+    </div>
+    </header>
+    <div id="contact-details-phone" class="form-section">
+        <div class="form-control">
+            <div class="label-wrapper">
+                <label for="contact-details-phone-work-field">work</label>
+            </div>
+            <div class="element-wrapper">
+                <input disabled id="contact-details-phone-work-field" type="tel" value="${contactData.phone.work}"/>
+            </div>
+        </div>
+        <div class="form-control">
+            <div class="label-wrapper">
+                <label for="contact-details-phone-mobile-field">mobile</label>
+            </div>
+            <div class="element-wrapper">
+                <input disabled id="contact-details-phone-mobile-field" type="tel" value="${contactData.phone.private}"/>
+            </div>
+        </div>
+        </div>
+        <div id="contact-details-mail" class="form-section">
+            <div class="form-control">
+                <div class="label-wrapper">
+                    <label for="contact-details-mail-work-field">work</label>
+                </div>
+                <div class="element-wrapper">
+                    <input disabled id="contact-details-mail-work-field" type="email" value="${contactData.mail.work}"/>
+                </div>
+            </div>
+            <div class="form-control">
+                <div class="label-wrapper">
+                    <label for="contact-details-mail-private-field">private</label>
+                </div>
+                <div class="element-wrapper">
+                    <input disabled id="contact-details-mail-private-field" type="email" value="${contactData.mail.private}"/>
+                </div>
+            </div>
+        </div>
+        <div id="contact-details-address" class="form-section">
+            <div class="form-control">
+                <div class="label-wrapper">
+                    <label for="contact-details-address-field-line-1">home</label>
+                </div>
+                <div class="element-wrapper">
+                    <input disabled id="contact-details-address-field-line-1" title="street and housenumber" type="text" value="${contactData.address.street}"/>
+                </div>
+                <div class="element-wrapper">
+                    <input disabled id="contact-details-address-field-line-2" title="city" type="text" value="${contactData.address.city}"/>
+                </div>
+                <div class="element-wrapper">
+                    <input disabled id="contact-details-address-field-line-3" title="country" type="text" value="${contactData.address.country}"/>
+                </div>
+            </div>
+        </div>
+        <div id="contact-details-note" class="form-section">
+            <div class="label-wrapper">
+                <label for="contact-details-note-field">note</label>
+            </div>
+            <div class="element-wrapper">
+            <textarea disabled id="contact-details-note-field">${note}</textarea>
+        </div>
+    </div>`;
+
+    /**
+     * Replace current form with markup form
+     * */
+    const detailsWrap = document.getElementById('contact-details-wrap');
+    const detailsFormCurrent = detailsWrap.querySelector('form#contact-details');
+
+    /* Keep DOM from repainting every individual element */
+    const detailsFormFragment = document.createDocumentFragment();
+
+    /* Rebuild form element in DOM */
+    const detailsFormNew = document.createElement('form');
+    detailsFormNew.id = 'contact-details';
+    detailsFormNew.className = 'read-mode';
+    detailsFormNew.innerHTML = markupDetail;
+
+    /* Append new form element to fragment */
+    detailsFormFragment.appendChild(detailsFormNew);
+    detailsWrap.replaceChild(detailsFormNew, detailsFormCurrent);
+
+    /* Set global form node variable to new form node */
+    pcard.form.element = detailsFormNew;
+
+    /**
+     * Update contact id
+     * */
+    detailsWrap.setAttribute('data-id', contactId);
+
+    /**
+     * Update current form actions
+     * */
+    const formActionCall = document.getElementById('contact-details-actions-call');
+    if (contactData.phone.private !== '') {
+        formActionCall.href = `tel:${contactData.phone.private}`;
+        formActionCall.removeAttribute('disabled');
+    } else {
+        formActionCall.href = '';
+        formActionCall.setAttribute('disabled', '');
+    }
+
+    const formActionMail = document.getElementById('contact-details-actions-mail');
+    if (contactData.mail.private !== '') {
+        formActionMail.href = `tel:${contactData.mail.private}`;
+        formActionMail.removeAttribute('disabled');
+    } else {
+        formActionMail.removeAttribute('href');
+        formActionMail.setAttribute('disabled', '');
+    }
 };
 
-const getContact = () => {
-    console.log('getting contact!');
-    showContact();
+const getContact = (contactTrigger) => {
+    const contactId = contactTrigger.getAttribute('data-id');
+    // console.log(`getting contact ${contactId}!`);
+
+    contacts.then((data) => {
+        const jsonData = JSON.parse(data);
+        const contactData = jsonData[contactId];
+
+        showContact(contactData, contactId);
+    });
 };
 
 const showContactList = () => {
-    console.log('showing contact list!');
+    // console.log('showing contact list!');
 };
 
 const getContactList = () => {
-    console.log('getting contact list!');
-
-    contacts.then((data) => {
-        console.log(JSON.parse(data));
-    });
-
+    // console.log('getting contact list!');
+    // contacts.then((data) => {
+    //     onsole.log(JSON.parse(data));
+    // });
     // showContactList();
 };
 
-function onViewChange(evt, contactTrigger) {
-    console.log('change view!');
-    console.log(evt);
+function onViewChange(contactTrigger) {
+    // console.log('change view!');
     // TODO: enhance class toggle logic for devices which use sm and md/lg media queries
     // TODO: use js media query class and contactTrigger argument for logic
     const container = document.querySelector('main');
     container.classList.toggle('view-change');
 
-    // TODO: get contact if contactTrigger argument is passed
+    // TODO: do not trigger getContact if contactTrigger has the same id as the current contact detail view
     if (contactTrigger) {
         getContact(contactTrigger);
     }
@@ -120,7 +267,7 @@ function toggleView() {
 
     // When you click a list item, bring on the details view.
     for (let i = 0; i < listItems.length; i++) {
-        listItems[i].addEventListener('click', e => onViewChange(e, listItems[i]), false);
+        listItems[i].addEventListener('click', e => onViewChange(listItems[i]), false);
     }
 
     // And switch it back again when you click the back button
